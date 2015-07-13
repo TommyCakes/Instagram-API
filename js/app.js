fced6c256eda5dd5a5d36031797967e58346bd95
TommyCakes <tomhwarhurst@gmail.com>
2015-07-13 12:25:51 +0100

    Rough Design/Popular pictures

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// + "<img class='picture' src =" 
+//         + data[i].images.standard_resolution.url + ">" ).show();
+
+
+
+$(document).ready(function() {
+
+  $('.getVideos').submit(function(event) {
+    $('#searchResults').html('');
+
+    var video = $(this).find("input[name='videos']").val();
+  });
+
+
+  $.ajax({
+    type:"GET",
+    url: "https://api.instagram.com/v1/tags/video/media/recent?access_token=341290126.e6b92ba.ef366927f98e43e2a9fc5b855da43628&max_tag_id=1026669534125882303 " , 
+    dataType: 'jsonp',
+    limit: 60,
+    success: function(data) {
+      console.log(data);
+      console.log(data.data[0].videos.low_resolution.url);
+
+      var data = data.data;
+
+      for(var i = 0; i < 30; i++) {
+        $('.popular').append("<li>" + "<p class='user'> Username: " + "<b>" + data[i].user.full_name + "</b></p>" + "<a target='_blank' href='" + data[i].link +
+        "'><img src='" + data[i].images.low_resolution.url + "'></img></a></li>");
+      }
+      
+
+
+   //      $('#submit').click(function() {
+   //      $.each(data, function(i, item) {
+
+   //      var picture = data[i].images.standard_resolution.url;
+       
+   //      $('.searchResults').append('<div id="results">' + "<h2 id='user'>" + '<p>Username:</p>' 
+   //      + data[i].user.full_name + "</h2>" + "<img src =" 
+   //      + data[i].user.profile_picture + ">"
+   //      + "<img class='picture' src =" 
+   //      + picture + ">" ).show();
+   //     console.log(picture.length);
+   //      if (picture.length < 20)
+   //        picture ++
+   // })  
+   //    });
+  // });
+}
+})
+ });
