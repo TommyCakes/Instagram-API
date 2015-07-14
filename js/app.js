feb9c5a6857ac2bdc3e6a74e7d25a7b73162b69c
TommyCakes <tomhwarhurst@gmail.com>
2015-07-14 09:54:02 +0100

    Added white space removal on string

--- js/app.js
+++ js/app.js
@@ -4,48 +4,89 @@
 
 
 $(document).ready(function() {
+  var nextURL;
+
 
   $('.getVideos').submit(function(event) {
-    $('#searchResults').html('');
-
+    $('.popular').html('');
     var video = $(this).find("input[name='videos']").val();
+    $.trim(video.replace(/\s+/g, ''));
+        console.log(video);
+        console.log($.trim(video.replace(/\s+/g, ''))); 
+
+$.ajax({
+    type:"GET",
+    url: "https://api.instagram.com/v1/tags/" + video + "/media/recent?access_token=341290126.e6b92ba.ef366927f98e43e2a9fc5b855da43628&max_tag_id=1026669534125882303" , 
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log(response);
+
+
+      var data = response.data;
+      nextURL = response.pagination.next_url
+
+
+      
+  $.each(data, function(i, val) {
+    if (val.type == "video"){
+   
+      $('.popular').append("<li>" + "<p class='user'> Username: " + "<b>" + val.user.full_name + "</b></p>" + "<a target='_blank' href='" + val.link +
+       "'><video src='" + val.videos.standard_resolution.url + "' class='vid' controls></video></a></li>");
+
+    }
+  })
+
+
+    if($('.popular').html() == ''){
+      if(response.pagination.next_url) {
+
+
+    $('.popular').html('<h2> No Videos can be displayed, please hit next </h2>')
+    }
+    else  {
+        $('.popular').html('<h2 id="none"> There are no more video matching this hastag!</h2>')
+        }
+      }
+    }
   });
+})
 
-
+$('#next').click(function() {
   $.ajax({
     type:"GET",
-    url: "https://api.instagram.com/v1/tags/video/media/recent?access_token=341290126.e6b92ba.ef366927f98e43e2a9fc5b855da43628&max_tag_id=1026669534125882303 " , 
+    url: nextURL,
     dataType: 'jsonp',
-    limit: 60,
-    success: function(data) {
-      console.log(data);
-      console.log(data.data[0].videos.low_resolution.url);
+    
+    success: function(response) {
+      $('.popular').html('')
+      console.log(response);
 
-      var data = data.data;
+      var data = response.data;
+      nextURL = response.pagination.next_url
 
-      for(var i = 0; i < 30; i++) {
-        $('.popular').append("<li>" + "<p class='user'> Username: " + "<b>" + data[i].user.full_name + "</b></p>" + "<a target='_blank' href='" + data[i].link +
-        "'><img src='" + data[i].images.low_resolution.url + "'></img></a></li>");
-      }
-      
+    $.each(data, function(i, val) {
+      if (val.type == "video"){
+        var user = val.user.full_name;
 
+       $('.popular').append("<li>" + "<p class='user'> Username: " + "<b>" + user + "</b></p>" + "<a target='_blank' href='" + val.link +
+      "'><video src='" + val.videos.standard_resolution.url + "' class='vid' controls></video></a></li>");
 
-   //      $('#submit').click(function() {
-   //      $.each(data, function(i, item) {
+    }
+  })
 
-   //      var picture = data[i].images.standard_resolution.url;
-       
-   //      $('.searchResults').append('<div id="results">' + "<h2 id='user'>" + '<p>Username:</p>' 
-   //      + data[i].user.full_name + "</h2>" + "<img src =" 
-   //      + data[i].user.profile_picture + ">"
-   //      + "<img class='picture' src =" 
-   //      + picture + ">" ).show();
-   //     console.log(picture.length);
-   //      if (picture.length < 20)
-   //        picture ++
-   // })  
-   //    });
-  // });
+  if($('.popular').html() == ''){
+    if(response.pagination.next_url){
+      $('.popular').html('<h2> No Videos can be displayed, please hit next</h2>')
+    }
+    else {
+      $('.popular').html('<h2 id="none"> There are no more video matching this hastag!</h2>')
+  }
+}
+
 }
 })
+});
  });
+
+    
+
